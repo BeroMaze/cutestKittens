8d353c6800a266054f4428a1e4831eccbd238e2d
BeroMaze <Berning.corey@gmail.com>
2015-12-11 16:12:27 -0600

    finished page

--- kitten.js
+++ kitten.js
@@ -5,16 +5,15 @@ var localData = localStorage.getItem('localKittenCorral'); // retreaving local i
 var kittenCorralLocal = JSON.parse(localData); // moving string info to object
               // console.log(kittenCorralLocal);
 
+
 kittenCorralLocal.forEach(function(k, w){
      kittenCorral[w].count = k.count + kittenCorral[w].count;
 });
 
 // event listener to run the first image function if clicked
-// $click1 = $('#img1').on('click',
   var clickOneFun = function () {
         kitten1.count++; // adds one to the kitten object that was picked
-      // localKittenInfo = JSON.stringify(kittenCorral); // stores new information to local storage
-      // localStorage.setItem('localKittenCorral', localKittenInfo); // stores new information to local storage
+        kitten1.voted = true; // turns kitten picked to true so it cant be voted on again
         $kittenLeft = $('#kitten1'); // links picture ones id
         $kittenLeft.css('border-color', 'green'); // changes picture ones border to green
         $kittenLeft.css('border-width', '10px'); // changes picture ones border to be thicker
@@ -42,8 +41,7 @@ $click1 = $('#img1').on('click', clickOneFun);
 // $click2 = $('#img2').on('click',
 var clickTwoFun = function () {
         kitten2.count++; // counts up by one for the image choosen
-      // localKittenInfo = JSON.stringify(kittenCorral); // stores new information to local storage
-      // localStorage.setItem('localKittenCorral', localKittenInfo); // stores new information to local storage
+        kitten2.voted = true;
         $kittenRight = $('#kitten2'); // links picture two id
         $kittenRight.css('border-color', 'green'); // changes picture twos border to green
         $kittenRight.css('border-width', '10px'); // changes picture twos border to thicker
@@ -92,24 +90,73 @@ var chart = function(){
 
 // event listener for the next button
 $('#next').on('click', function () {
-      $('#img1').empty(); // empties picture one
-      $('#img2').empty(); // empties picture two
-      $('#button').remove(); // removes rthe next button
-      $kittenLeft = $('#kitten1'); // // links to image one
-      $kittenLeft.css('border-color', 'white'); // changes image ones border to white
-      $kittenLeft.css('border-width', '3px'); // changes image ones border to thin
-      $kittenLeft.css('background-color', 'black'); // changes image ones background to black
-      $kittenRight = $('#kitten2'); // links to image two
-      $kittenRight.css('border-color', 'white'); // changes image two border to white
-      $kittenRight.css('border-width', '3px'); // changes image two border to thin
-      $kittenRight.css('background-color', 'black'); // changes image twos baclground to white
-      chooseOne(); // runs the first kitten image choose
-      chooseTwo(); // runs the second kitten image choose
-      $('#img1').on('click', clickOneFun);
-      $('#img2').on('click', clickTwoFun);
-      window.scrollTo(0, 0); // scrolls screen to the top
-  });
+      timesVoted++;
+      if(timesVoted === 13){
+          $('#button').remove();
+          $('#img1').remove(); // empties picture one
+          $('#img2').remove(); // empties picture two
+          $('#kitten1').remove();
+          $('#kitten2').remove();
+          $('#name1').remove();
+          $('#name2').remove();
+          $('#pawsLeft').remove();
+          $('#pawsRight').remove();
+          $('#done').text('Thank you for voting for the Cutest Kitten!');
+          $('#check').text('Check out the results so far:');
+          window.scrollTo(0, 0);
+          // $('#finished').html("<div class='fin'><img src='./kittens/1.jpg' class='finishedImg'><p4>Kitten 1</p4></div> \
+          //                     <div class='fin'><img src='./kittens/2.jpg' class='finishedImg'><p4>Kitten 2</p4></div> \
+          //                     <div class='fin'><img src='./kittens/3.jpg' class='finishedImg'><p4>Kitten 3</p4></div> \
+          //                     <div class='fin'><img src='./kittens/4.jpg' class='finishedImg'><p4>Kitten 4</p4></div> \
+          //                     <div class='fin'><img src='./kittens/5.jpg' class='finishedImg'><p4>Kitten 5</p4></div> \
+          //                     <div class='fin'><img src='./kittens/6.jpg' class='finishedImg'><p4>Kitten 6</p4></div> \
+          //                     <div class='fin'><img src='./kittens/7.jpg' class='finishedImg'><p4>Kitten 7</p4></div> \
+          //                     <div class='fin'><img src='./kittens/8.jpg' class='finishedImg'><p4>Kitten 8</p4></div> \
+          //                     <div class='fin'><img src='./kittens/9.jpg' class='finishedImg'><p4>Kitten 9</p4></div> \
+          //                     <div class='fin'><img src='./kittens/10.jpg' class='finishedImg'><p4>Kitten 10</p4></div> \
+          //                     <div class='fin'><img src='./kittens/11.jpg' class='finishedImg'><p4>Kitten 11</p4></div> \
+          //                     <div class='fin'><img src='./kittens/12.jpg' class='finishedImg'><p4>Kitten 12</p4></div> \
+          //                     <div class='fin'><img src='./kittens/13.jpg' class='finishedImg'><p4>Kitten 13</p4></div> \
+          //                     <div class='fin'><img src='./kittens/14.jpg' class='finishedImg'><p4>Kitten 14</p4></div>");
 
+          $('#finishLeft').html("<div class='fin'><img src='./kittens/1.jpg' class='finishedImg'><p4>Kitten 1</p4></div> \
+                              <div class='fin'><img src='./kittens/2.jpg' class='finishedImg'><p4>Kitten 2</p4></div> \
+                              <div class='fin'><img src='./kittens/3.jpg' class='finishedImg'><p4>Kitten 3</p4></div> \
+                              <div class='fin'><img src='./kittens/4.jpg' class='finishedImg'><p4>Kitten 4</p4></div> \
+                              <div class='fin'><img src='./kittens/5.jpg' class='finishedImg'><p4>Kitten 5</p4></div> \
+                              <div class='fin'><img src='./kittens/6.jpg' class='finishedImg'><p4>Kitten 6</p4></div> \
+                              <div class='fin'><img src='./kittens/7.jpg' class='finishedImg'><p4>Kitten 7</p4></div>");
+          $('#finishRight').html("<div class='fin'><img src='./kittens/8.jpg' class='finishedImg'><p4>Kitten 8</p4></div> \
+                              <div class='fin'><img src='./kittens/9.jpg' class='finishedImg'><p4>Kitten 9</p4></div> \
+                              <div class='fin'><img src='./kittens/10.jpg' class='finishedImg'><p4>Kitten 10</p4></div> \
+                              <div class='fin'><img src='./kittens/11.jpg' class='finishedImg'><p4>Kitten 11</p4></div> \
+                              <div class='fin'><img src='./kittens/12.jpg' class='finishedImg'><p4>Kitten 12</p4></div> \
+                              <div class='fin'><img src='./kittens/13.jpg' class='finishedImg'><p4>Kitten 13</p4></div> \
+                              <div class='fin'><img src='./kittens/14.jpg' class='finishedImg'><p4>Kitten 14</p4></div>");
+
+          console.log(kittenCorral);
+
+      }
+      else{
+          $('#myChart').remove();
+          $('#img1').empty(); // empties picture one
+          $('#img2').empty(); // empties picture two
+          $('#button').remove(); // removes the next button
+          $kittenLeft = $('#kitten1'); // // links to image one
+          $kittenLeft.css('border-color', 'white'); // changes image ones border to white
+          $kittenLeft.css('border-width', '3px'); // changes image ones border to thin
+          $kittenLeft.css('background-color', 'black'); // changes image ones background to black
+          $kittenRight = $('#kitten2'); // links to image two
+          $kittenRight.css('border-color', 'white'); // changes image two border to white
+          $kittenRight.css('border-width', '3px'); // changes image two border to thin
+          $kittenRight.css('background-color', 'black'); // changes image twos baclground to white
+          chooseOne(); // runs the first kitten image choose
+          chooseTwo(); // runs the second kitten image choose
+          $('#img1').on('click', clickOneFun);
+          $('#img2').on('click', clickTwoFun);
+          window.scrollTo(0, 0); // scrolls screen to the top
+      }
+  });
     window.onbeforeunload = function() {
       localKittenInfo = JSON.stringify(kittenCorral); // stores new information to local storage
       localStorage.setItem('localKittenCorral', localKittenInfo); // stores new information to local storage
--- objects.js
+++ objects.js
@@ -3,13 +3,14 @@ var Kittens = function(name, fileName){
     this.name = name;
     this.image = fileName;
     this.count = 0;
+    this.voted = false;
 }
 //the number for the first/second number generated and first/second kitten chosen
 var number1;
 var kitten1;
 var number2;
 var kitten2;
-var kittenVoted = [];
+var timesVoted = 0;
 
 // each kitten object for each picture
 var one = new Kittens('one', '1.jpg');
@@ -35,83 +36,41 @@ var chooseOne = function(){
     number1 = Math.floor(Math.random() * 14) + 1; // random number generated
     kitten1 = kittenCorral[number1 - 1]; // picks the object in the array
 
-
-    kittenVoted.forEach(function(i){
-          if(kitten1.image === kittenVoted.image){
-            chooseOne();
-          }
-    });
-
-      // for(var i=0; i < kittenVoted.length; i++){
-      //         if(kitten1.image === kittenVoted[i].image){
-      //           chooseOne();
-      //         }
-      //       }
-    // });
-                kittenVoted.push(kitten1.image);
-                // console.log(kitten1.name);
-                console.log(kittenVoted);
-
-    // pushes image into the first image slot
-    var img1 = $('<img />', // image tag
+    if(kitten1.voted === false){
+      var img1 = $('<img />', // image tag
              { class: 'image', // image one id
                src: 'kittens/' + kitten1.image, // source to the image used
              }).appendTo('#img1'); // appends image to slot with size
+    $('#name1').text(kitten1.name);
+    }
+    else{
+      chooseOne();
+    }
 }
 
 // number generator for the second kitten image
 var chooseTwo = function(){
     number2 = Math.floor(Math.random() * 14) + 1; // random number generated
         if (number2 === number1){ // checks to see if image 1 and 2 are the same
-              newNumber2 = Math.floor(Math.random() * 14) + 1; // generates new number if needed
-            kitten2 = kittenCorral[newNumber2 - 1]; // picks the object in the array
-
-          kittenVoted.forEach(function(i){
-      if(kitten2.image === kittenVoted.image){
-        chooseTwo();
-      }
-});
-
-          // for(var i=0; i < kittenVoted.length; i++){
-          //     if(kitten2.image === i.image){
-          //       chooseTwo();
-          //     }
-          // }
-            // });
-                kittenVoted.push(kitten2.image);
-                // console.log(kitten1.name);
-                console.log(kittenVoted);
-
-            var img2 = $('<img />', // image tag
-                  { class: 'image', // image two id
-                    src: 'kittens/' + kitten2.image, // source to the image used
-                  }).appendTo('#img2'); // appends image to slots with size
+              chooseTwo();
         }
 
         else {
             kitten2 = kittenCorral[number2 - 1]; // picks the objects in the array
 
-        kittenVoted.forEach(function(i){
-            if(kitten2.image === kittenVoted.image){
-              chooseTwo();
-            }
-        });
-            // for(var i=0; i < kittenVoted.length; i++){
-            //   if(kitten2.image === i.image){
-            //     chooseTwo();
-            //   }
-            // }
-            // });
-                kittenVoted.push(kitten2.image);
-                // console.log(kitten1.name);
-                console.log(kittenVoted);
-
-               var img2 = $('<img />', // image tag
+              if (kitten2.voted === false){
+                  var img2 = $('<img />', // image tag
                      { class: 'image', // image two id
                        src: 'kittens/' + kitten2.image, // source to the image
                      }).appendTo('#img2'); // chooses the kitten object
+                  $('#name2').text(kitten2.name);
+              }
+              else{
+              chooseTwo();
+              }
         }
 }
+
 
 
 
